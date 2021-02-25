a99e513fa0dfe631645bb260ced05f716a2d75fb
Joseph Monica <joseph.rxjs.it@gmail.com>
2021-02-25 16:14:26 +0700

    init custom module

---
+++ addons/school/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'School',
+    'version': '14.0.1.0.0',
+    'depends': ['base'],
+    'author': 'Development Team',
+    'maintainer': 'Development Team',
+    'company': 'SDC',
+    'category': 'Tools',
+    'description': 'Module Description',
+    'data': [
+        'views/student_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3'
+}
